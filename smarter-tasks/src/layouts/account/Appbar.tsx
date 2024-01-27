ac6c0550e4f44f24f4f42700740132ea1c377e11
gautam-bhagat <gautam.bhagat22@pccoepune.org>
2024-01-27 19:21:22 +0530

    Eslint ignore added to appbar file

--- smarter-tasks/src/layouts/account/Appbar.tsx
+++ smarter-tasks/src/layouts/account/Appbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
@@ -24,6 +25,7 @@ const Appbar = () => {
   return (
     <>
       <Disclosure as="nav" className="border-b border-slate-200">
+        
         {({ open }) => (
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
