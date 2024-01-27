c5862b699b786741d21fb9986c66519a222d7f87
gautam-bhagat <gautam.bhagat22@pccoepune.org>
2024-01-27 18:27:25 +0530

    L6 Ready V1

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
+interface UserData {
+  createdAt: string;
+  email: string;
+  id: number;
+  name: string;
+  organisation_id: number;
+  updatedAt: string;
+}
 const Dashboard: React.FC = () => {
+
+    const storedData: string | null = localStorage.getItem("userData");
+    const userData: UserData = JSON.parse(storedData!) as UserData;
+
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className=" w-1/2 px-6 py-8 bg-white flex flex-col justify-center rounded-lg shadow-md">
+          <p>Name : {userData.name}</p>
+          <p>Email : {userData.email}</p>
+          <a id="logout-link" href="/" className="w-1/2 bg-gray-700 text-center hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4 mx-auto">Logout
+          </a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
